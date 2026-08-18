1b8527a778fff4ee57c57521c59ac4313feb5910
zakariyasabra <zakariyasabra@gmail.com>
2026-08-18 03:57:43 +0300

    Create google-analytics.tsx

---
+++ src/components/analytics/google-analytics.tsx
@@ -0,0 +1,13 @@
+<head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-NZNKNBEX66"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-NZNKNBEX66');
+  </script>
+
+  ...
+</head>
